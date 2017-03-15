953045006e37e38fbb75619d92d285962c7d804d
beingriant <beingriant@gmail.com>
2017-03-15 13:23:24 +0700

    ad.js

--- js/ad.js
+++ js/ad.js
@@ -1,4 +1,4 @@
-$(function() {
+(function($) {
     $('#mArticle .area_view img').first().after('<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8114152585404854" data-ad-slot="7469071931" data-ad-format="link"></ins>')
     (adsbygoogle = window.adsbygoogle || []).push({});
-});
+})(jQuery);
