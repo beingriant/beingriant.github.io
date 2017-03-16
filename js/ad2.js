56c316747f1fd072c9d9ab983d429a78d619ab8d
beingriant <beingriant@gmail.com>
2017-03-16 09:42:30 +0700

    ad2.js

--- js/ad2.js
+++ js/ad2.js
@@ -1,4 +1,4 @@
 (function($) {
-    $('#mArticle img').first().after('<ins class="adsbygoogle" style="display:inline-block;width:320px;height:100px" data-ad-client="ca-pub-8114152585404854" data-ad-slot="2384567530"></ins>');
+    $('#mArticle p').eq(2).after('<ins class="adsbygoogle" style="display:inline-block;width:320px;height:100px" data-ad-client="ca-pub-8114152585404854" data-ad-slot="2384567530"></ins>');
     (adsbygoogle = window.adsbygoogle || []).push({});
 })(jQuery);
