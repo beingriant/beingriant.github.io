9096032dba8ff9d8d336525f59d8a0a5eeda0bce
beingriant <beingriant@gmail.com>
2017-03-27 16:04:27 +0900

    rank.js

--- js/rank.js
+++ js/rank.js
@@ -1,12 +1,12 @@
 (function($) {
     $('.div-news').each(function(index) {
-        if (index == 3 ) {
+        if (index == 2 ) {
             $(this).after('<p style="text-align: center;"><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8114152585404854" data-ad-slot="5308217531" data-ad-format="auto"></ins></p>');
             (adsbygoogle = window.adsbygoogle || []).push({});
-        } else if (index == 8) {
+        } else if (index == 5) {
             $(this).after('<p style="text-align: center;"><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8114152585404854" data-ad-slot="6784950732" data-ad-format="auto"></ins></p>');
             (adsbygoogle = window.adsbygoogle || []).push({});
-        } else if (index == 13) {
+        } else if (index == 8) {
             $(this).after('<p style="text-align: center;"><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8114152585404854" data-ad-slot="8261683938" data-ad-format="auto"></ins></p>');
             (adsbygoogle = window.adsbygoogle || []).push({});
         }
