a98eda3e297d4a4c0830f7716acd9257af1c9cab
beingriant <beingriant@gmail.com>
2017-03-27 16:01:51 +0900

    rank.js

--- js/rank.js
+++ js/rank.js
@@ -1,13 +1,13 @@
 (function($) {
     $('.div-news').each(function(index) {
         if (index == 3 ) {
-            $(this).after('<p style="text-align: center;"><ins class="adsbygoogle" style="display:inline-block;width:320px;height:100px" data-ad-client="ca-pub-8114152585404854" data-ad-slot="5308217531"></ins></p>');
+            $(this).after('<p style="text-align: center;"><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8114152585404854" data-ad-slot="5308217531" data-ad-format="auto"></ins></p>');
             (adsbygoogle = window.adsbygoogle || []).push({});
         } else if (index == 8) {
-            $(this).after('<p style="text-align: center;"><ins class="adsbygoogle" style="display:inline-block;width:320px;height:100px" data-ad-client="ca-pub-8114152585404854" data-ad-slot="6784950732"></ins></p>');
+            $(this).after('<p style="text-align: center;"><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8114152585404854" data-ad-slot="6784950732" data-ad-format="auto"></ins></p>');
             (adsbygoogle = window.adsbygoogle || []).push({});
         } else if (index == 13) {
-            $(this).after('<p style="text-align: center;"><ins class="adsbygoogle" style="display:inline-block;width:320px;height:100px" data-ad-client="ca-pub-8114152585404854" data-ad-slot="8261683938"></ins></p>');
+            $(this).after('<p style="text-align: center;"><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8114152585404854" data-ad-slot="8261683938" data-ad-format="auto"></ins></p>');
             (adsbygoogle = window.adsbygoogle || []).push({});
         }
     });
